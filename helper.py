2eb0e8ded0d24417e86fd3252022aa2724b6be06
murtaza98 <m.patrawala@somaiya.edu>
2019-02-12 13:55:34 +0530

    Modified Helper file

--- helper.py
+++ helper.py
@@ -54,3 +54,22 @@ def sentences_to_indices(X, word_to_index, max_len):
             j += 1            
     
     return X_indices
+
+def read_glove_vecs(glove_file):
+    with open(glove_file, 'r') as f:
+        words = set()
+        word_to_vec_map = {}
+        for line in f:
+            line = line.strip().split()
+            curr_word = line[0]
+            words.add(curr_word)
+            word_to_vec_map[curr_word] = np.array(line[1:], dtype=np.float64)
+        
+        i = 1
+        words_to_index = {}
+        index_to_words = {}
+        for w in sorted(words):
+            words_to_index[w] = i
+            index_to_words[i] = w
+            i = i + 1
+    return words_to_index, index_to_words, word_to_vec_map
