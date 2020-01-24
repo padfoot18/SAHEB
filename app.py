2cbfd9b002fa701531ba79f7b31cb3c57cb9303c
murtaza98 <m.patrawala@somaiya.edu>
2020-01-24 19:22:56 +0530

    issue fixed: server loading twice

--- app.py
+++ app.py
@@ -451,4 +451,4 @@ api.add_resource(ChatBot, '/chat/')
 if __name__ == '__main__':
     app.secret_key = 'qwertyuuiopmkaejnfi;awnciquw4gabpiuebrjwabefiuawufbaeuhb'
     load_all_model()
-    app.run(host='127.0.0.1', port=5000, debug=True)
+    app.run(host='127.0.0.1', port=5000, debug=True, use_reloader=False)
